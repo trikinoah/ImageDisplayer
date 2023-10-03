58eb40b8a279081c938f47015a7af020e038be68
trikinoah <noah.triki@gmail.com>
2023-10-03 16:43:20 +0200

    feat(1.0): version 1.0

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -28,38 +28,10 @@ namespace ImageDisplayer
         public MainWindow()
         {
             InitializeComponent();
-
-            try
-            {
-
-
-                //browser.Visible = true;
-                //pdfWebViewer.Navigate(new Uri("about:blank"));
-
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("browser is visible/ not: ");
-
-            }
         }
 
         private async void Button_Click(object sender, RoutedEventArgs e)
         {
-            /*           // Create OpenFileDialog 
-                       Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
-
-
-
-                       // Set filter for file extension and default file extension 
-                       dlg.DefaultExt = ".png";
-                       dlg.Filter = "JPEG Files (*.jpeg)|*.jpeg|PNG Files (*.png)|*.png|JPG Files (*.jpg)|*.jpg|GIF Files (*.gif)|*.gif";
-
-
-                       // Display OpenFileDialog by calling ShowDialog method 
-                       Nullable<bool> result = dlg.ShowDialog();
-           */
-
             // Get the selected file name and display in a TextBox 
             string file_path = "C:\\Users\\Admin\\Documents\\Test";
             ChromeOptions options = new ChromeOptions();
@@ -67,8 +39,6 @@ namespace ImageDisplayer
 
 
             IWebDriver driver = new ChromeDriver(options);
-            var browser = (IJavaScriptExecutor)driver;
-
             while (true)
             {
                 string[] files = Directory.GetFiles(file_path);
@@ -80,7 +50,9 @@ namespace ImageDisplayer
                     if (parts.Length == 3 && int.TryParse(parts[1], out int durationSec) && int.TryParse(parts[2], out int zoom))
                     {
                         driver.Navigate().GoToUrl(file);
-                        browser.ExecuteScript("document.body.style.zoom = '0.5'");
+                        ExecuteJavaScript($"document.body.style.zoom = '{(zoom / 100.0)}'");
+
+
                         //System.Windows.Forms.SendKeys.SendWait("{F11}");
                         await Task.Delay(durationSec * 1000);
                     }
@@ -95,32 +67,13 @@ namespace ImageDisplayer
                             await Task.Delay(duration * 1000);
                         }
                     }
-                    
-
                 }
-
-                /*  driver.Navigate().GoToUrl("file:///C:/Users/Admin/Documents/rapport-annuel-danone-2019.pdf");
-
-                  var title = driver.Title;
-                  //Assert.AreEqual("Web form", title);
-
-                  driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromMilliseconds(5000);*/
             }
-            
-
-           /* var textBox = driver.FindElement(By.Name("my-text"));
-            var submitButton = driver.FindElement(By.TagName("button"));
-
-            textBox.SendKeys("Selenium");
-            submitButton.Click();
-
-            var message = driver.FindElement(By.Id("message"));
-            var value = message.Text;
-            //Assert.AreEqual("Received!", value);
-
-            driver.Quit();*/
-
-
+        }
+        private void ExecuteJavaScript(string script)
+        {
+            IJavaScriptExecutor jsExecutor = (IJavaScriptExecutor)driver;
+            jsExecutor.ExecuteScript(script);
         }
     }
 }
